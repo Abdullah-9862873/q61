5a6fe861538be67a05916cd6613ce82c92a357bb
shahzaibnawaz902@gmail.com <shahzaibnawaz902@gmail.com>
2021-11-22 09:24:41 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	//Question 61
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b, c, d, e;
+			a = 10; b = 20; c = 15; d = 8; e = 40;
+
+			{
+				//Part1
+
+				int f = (a + b / (c - 5)) / ((d + 7) / (e - 37) % 3);
+				Console.WriteLine(f);
+				Console.ReadLine();
+			}
+			{
+				//Part 2
+				int f = a + b / c - 5 / d + 7 / e - 37 % 3;
+				Console.WriteLine(f);
+				Console.ReadLine();
+			}
+			{
+				//Part 3
+				int f = a * (b * b) - (c * b) + d;
+				Console.WriteLine(f);
+				Console.ReadLine();
+			}
+		}
+	}
+}
